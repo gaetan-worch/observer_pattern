9a2260711207d0276ae88e03154346aa52523de2
Gaetan WORCH <gaetan.worch@heig-vd.ch>
2022-12-06 15:25:18 +0100

    virtual fonction done

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
+#include <iostream>
+
 class ISubscriber(){
 
 };
 
 class IPublisher(){
-
+    public :
+    virtual void subscribe() = 0;
+    virtual void notifySubscriber() = 0;
 };
 
 class User(){
 
 };
 
-class News(){
-
+class News() : public ISubscriber{
+    
 };
 
 int main(){
